ac8c8e601c8e5bc67757b94611ee118244bb23ee
impress_pci <pci2676@gmail.com>
2018-08-24 19:06:47 +0900

    spring annotation property but has a problem about cannot open a properties file

---
+++ Ex_Spring_Annotation_Property/src/main/java/com/gmail/pci2676/MainClass.java
@@ -0,0 +1,21 @@
+package com.gmail.pci2676;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		AdminConnection connection = ctx.getBean("adminConfig",AdminConnection.class);
+		
+		System.out.println("adminID : "+connection.getAdminId());
+		System.out.println("adminPW : "+connection.getAdminPw());
+		System.out.println("sub_adminID : "+connection.getSub_adminId());
+		System.out.println("sub_adminPW : "+connection.getSub_adminPw());
+		
+		ctx.close();
+	}
+
+}
